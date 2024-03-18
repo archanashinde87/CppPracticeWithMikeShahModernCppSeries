b14bf0a13651cfd311e838d916013af6df1ecfab
Archana Shinde <144507091+archanashinde87@users.noreply.github.com>
2024-03-18 16:59:51 +0530

    Add files via upload
    simple while loop example

---
+++ whileloop.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int countdown = 3;
+    while(countdown > 0)
+    {
+        std::cout << countdown << std::endl;
+        countdown--;
+    }
+   return 0;
+}
